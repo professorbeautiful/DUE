54f87caa8a5352d60b69aaa41b7c6a68e180a7d1
Roger Day <notsofeo@gmail.com>
2017-06-30 12:01:12 -0400

    Numerous additions to the interface, lower left side.

--- inst/app.R
+++ inst/app.R
@@ -9,8 +9,8 @@
 
 library(shiny)
 library(shinyDebuggingPanel)
-library(DUE)
 library(shinyBS)
+library(DUE)
 
 ui <- fluidPage(
   
@@ -23,18 +23,32 @@ ui <- fluidPage(
       column(6,
              plotOutput("linePlot")
       )), 
-    fluidRow(
-        column(6, "insert graph here"), 
-        column(6,
-              bsButton(inputId="Additive", "Additive"),
-              bsButton(inputId="Simple", "Simple"),
-              bsButton(inputId="Cautious", "Cautious"),
-              bsButton(inputId="Aggressive", "Aggressive")
-        ))
+  fluidRow(
+    column(6, "insert graph here"), 
+    column(3,
+           fluidRow(
+                  bsButton(inputId="Additive", "Additive"),
+                  bsButton(inputId="Simple", "Simple")),
+           fluidRow(
+                  bsButton(inputId="Cautious", "Cautious"),
+                  bsButton(inputId="Aggressive", "Aggressive"))
+    ),
+    column(3,
+           "Responsive, but not yet activated",
+           fluidRow(
+                  numericInput(inputId="U.rt", "U.rt", value=0),
+                  numericInput(inputId="U.rT", "U.rT", value=0)),
+           fluidRow(
+                  numericInput(inputId="U.Rt", "U.Rt", value=0),
+                  numericInput(inputId="U.RT", "U.RT", value=0))
+    )
   )
+)
 
 
 server <- function(input, output, session) {
+  shinyDebuggingPanel::makeDebuggingPanelOutput(session) 
+  
   source("plotProbsAndEUsimplified.R", local = TRUE)
   DUEstartShiny = 
     function () 
@@ -64,7 +78,11 @@ server <- function(input, output, session) {
   DUEget = function(objname) DUEenv[[objname]]
   DUEput = function(objname, value) 
     DUEenv[[objname]] = value
-  doseParameters(resetToDefaults = TRUE)
+  ## isolate({doseParameters(resetToDefaults = TRUE)})
+  ## Unknown why this call to doseParameters does not change DUEenv.
+  ## But this source'ing approach works.
+  source('doseParametersForApp.R', local=TRUE)
+  
   ##### Create Utility Choice Buttons ####
   isolate({
     DUEenv$utilityChoices <- list(
@@ -74,27 +92,59 @@ server <- function(input, output, session) {
       "Aggressive" = data.frame(U.rt=0, U.rT= -1,  U.Rt=1, U.RT= 1)
     )
     DUEenv$utilityChoiceNames <- names(DUEenv$utilityChoices)
+    DUEput('testing', 'testing')  ### OK
   })
   DUEstartShiny()
-  shinyDebuggingPanel::makeDebuggingPanelOutput(session) 
   observe({
-    input$Additive
-    TheseUvalues = DUEenv$utilityChoices$Additive
-    isolate ({
-      DUEenv$U.rt <- TheseUvalues$U.rt
-      DUEenv$U.Rt <- TheseUvalues$U.Rt
-      DUEenv$U.rT <- TheseUvalues$U.rT
-      DUEenv$U.RT <- TheseUvalues$U.RT
-    })
-    print("Finished Additive observer")
+    updateNumericInput(session=session, 'U.rt', value=DUEenv$U.rt)
+    updateNumericInput(session=session, 'U.Rt', value=DUEenv$U.Rt)
+    updateNumericInput(session=session, 'U.rT', value=DUEenv$U.rT)
+    updateNumericInput(session=session, 'U.RT', value=DUEenv$U.RT)
   })
-  observe({
-    input$Simple
-    TheseUvalues = DUEenv$utilityChoices$Simple
+  resetButtonStyles = function(whichButton) {
+    for(button in DUEenv$utilityChoiceNames) 
+      updateButton(session, button,
+                   style='default')
+    updateButton(session, whichButton,
+                 style='success')
+    }
+  updateUtilities = function(TheseUvalues) {
     DUEenv$U.rt = TheseUvalues$U.rt
     DUEenv$U.Rt = TheseUvalues$U.Rt
     DUEenv$U.rT = TheseUvalues$U.rT
     DUEenv$U.RT = TheseUvalues$U.RT
+  }
+  
+  
+  observe({
+    input$Additive
+    resetButtonStyles('Additive')
+    isolate ({
+      DUEenv$utility = TheseUvalues = 
+        DUEenv$utilityChoices$Additive
+      updateUtilities(TheseUvalues)
+    })
+  })
+  observe({
+    input$Simple
+    resetButtonStyles('Simple')
+    DUEenv$utility = TheseUvalues = 
+      DUEenv$utilityChoices$Simple
+    updateUtilities(TheseUvalues)
+  })
+  observe({
+    input$Cautious
+    resetButtonStyles('Cautious')
+    DUEenv$utility = TheseUvalues = 
+      DUEenv$utilityChoices$Cautious
+    updateUtilities(TheseUvalues)
+  }) 
+  observe({
+    input$Aggressive
+    resetButtonStyles('Aggressive')
+    DUEenv$utility = TheseUvalues = 
+      DUEenv$utilityChoices$Aggressive
+    updateUtilities(TheseUvalues)
   })
   output$linePlot <- renderPlot({
     plotProbsAndEUsimplified()
