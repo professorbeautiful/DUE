883e74134bc7da6c03e725c3b86523566bc92f5a
Roger Day <notsofeo@gmail.com>
2017-07-10 21:47:02 -0400

    Adding DUEenv arguments. I haven't figured out a slick way of reaching up to obtain DUEenv.

--- R/recalculate.means.and.variances.R
+++ R/recalculate.means.and.variances.R
@@ -1,23 +1,23 @@
 recalculate.means.and.variances <-
-function(){
-	for(i in 1:DUEenv$nPops) {
-		for(j in 1:nrow(DUEenv$the.variances.pop[[i]])) {
-			DUEenv$theLognormalParameters = FromNormalToLognormal(
-							DUEenv$the.Ethresholds.pop[[i]][j], 
-							DUEenv$the.CVs.pop[[i]][j])
-			DUEenv$the.means.pop[[i]][j] <- 
-				DUEenv$theLognormalParameters["uStar"]
-	#cat("recalculate.means.and.variances: the.means.pop[[i]] = ",   DUEenv$the.means.pop[[i]], "\n")
-			DUEenv$the.variances.pop[[i]][j,j] <- 
-				DUEenv$theLognormalParameters["vStar"]
-			if(DUEenv$the.variances.pop[[i]][j,j] < 1e-7 ) {
-				MINVARIANCE = 1e-2
-				cat("Bad value (zero) for jth diagonal of the.variances.pop[[", i, "]]\n")
-				DUEenv$the.variances.pop[[i]] [j,j] <- MINVARIANCE
-			}
-		}
-	}
-	recalculate.offdiagonals()
- #})
-}
+  function(DUEenv=DUEenv){
+    for(i in 1:DUEenv$nPops) {
+      for(j in 1:nrow(DUEenv$the.variances.pop[[i]])) {
+        DUEenv$theLognormalParameters = FromNormalToLognormal(
+          DUEenv$the.Ethresholds.pop[[i]][j], 
+          DUEenv$the.CVs.pop[[i]][j])
+        DUEenv$the.means.pop[[i]][j] <- 
+          DUEenv$theLognormalParameters["uStar"]
+        #cat("recalculate.means.and.variances: the.means.pop[[i]] = ",   DUEenv$the.means.pop[[i]], "\n")
+        DUEenv$the.variances.pop[[i]][j,j] <- 
+          DUEenv$theLognormalParameters["vStar"]
+        if(DUEenv$the.variances.pop[[i]][j,j] < 1e-7 ) {
+          MINVARIANCE = 1e-2
+          cat("Bad value (zero) for jth diagonal of the.variances.pop[[", i, "]]\n")
+          DUEenv$the.variances.pop[[i]] [j,j] <- MINVARIANCE
+        }
+      }
+    }
+    #recalculate.offdiagonals(theDUEenv=DUEenv)
+    #})
+  }
 
--- R/recalculate.offdiagonals.R
+++ R/recalculate.offdiagonals.R
@@ -1,20 +1,23 @@
-recalculate.offdiagonals <- function (correl, .thisPop) 
+recalculate.offdiagonals <- function (correl, .thisPop, theDUEenv=DUEenv) 
 {
-	#DUEget(c(nPops,the.correlations.pop,the.variances.pop,the.means.pop))
-    if (missing(.thisPop)) {
-        for (..thisPop in 1:DUEenv$nPops) recalculate.offdiagonals(.thisPop = ..thisPop)
-        return(NULL)
-    }
-    if (!missing(correl)) {
-        #cat("recalculate.offdiagonals:  correl=", correl, "\n")
-        DUEenv$the.correlations.pop[[DUEenv$thisPop]] <<- correl
-	print(DUEenv$the.correlations.pop)
-    }
-
-	DUEenv$the.variances.pop[[.thisPop]][2, 1] <<- DUEenv$the.variances.pop[[.thisPop]][1, 2] <<-
-		DUEenv$the.correlations.pop[[.thisPop]] *
-		sqrt(DUEenv$the.variances.pop[[.thisPop]][1, 1]) *
-		sqrt(DUEenv$the.variances.pop[[.thisPop]][2, 2])
-        return(NULL)
+  DUEenv = theDUEenv
+  #DUEenv = get('DUEenv', envir = parent.frame(2))
+  #DUEget(c(nPops,the.correlations.pop,the.variances.pop,the.means.pop))
+  if (missing(.thisPop)) {
+    for (..thisPop in 1:DUEenv$nPops) 
+      recalculate.offdiagonals(correl=correl, .thisPop = ..thisPop, theDUEenv=DUEenv )
+    return(NULL)
+  }
+  if (!missing(correl)) {
+    #cat("recalculate.offdiagonals:  correl=", correl, "\n")
+    DUEenv$the.correlations.pop[[DUEenv$thisPop]] <<- correl
+    print(DUEenv$the.correlations.pop)
+  }
+  
+  DUEenv$the.variances.pop[[.thisPop]][2, 1] <<- DUEenv$the.variances.pop[[.thisPop]][1, 2] <<-
+    DUEenv$the.correlations.pop[[.thisPop]] *
+    sqrt(DUEenv$the.variances.pop[[.thisPop]][1, 1]) *
+    sqrt(DUEenv$the.variances.pop[[.thisPop]][2, 2])
+  return(NULL)
 }
 
