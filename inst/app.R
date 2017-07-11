349cb71fa3e7b7d40fdb3de7223394bc4936c692
Roger Day <notsofeo@gmail.com>
2017-07-11 15:26:06 -0400

    Various cosmetic improvements. Also added a 'click' to the threshold plot.

--- inst/app.R
+++ inst/app.R
@@ -14,120 +14,138 @@ rt.outcome.colors = DUEenvironmentDefault$rt.outcome.colors
 make_linethicknessButton = function(labelNum)
   column(1,
          tagAppendAttributes(
-         bsButton(paste0('linethickness_', label<-probLineNames[labelNum]), label=label),
-                  style=paste0('text-color:', rt.outcome.colors[labelNum]) ) )
+           bsButton(paste0('linethickness_', label<-probLineNames[labelNum]), label=label),
+           style=paste0('text-color:', rt.outcome.colors[labelNum]) ) )
 linethicknessButtons = 
   lapply(1:length(probLineNames), make_linethicknessButton)   
 print(linethicknessButtons)
 
 ui <- fluidPage(
   includeCSS('DUE.css'),
-  titlePanel(paste("DUE Shiny app: date = ",
-                   desc$Date, "  Version = ", desc$Version)),
+  titlePanel(div( style='text-align:center; color:blue;', 
+             paste("DUE Shiny app: date = ",
+                   desc$Date, "  Version = ", desc$Version))),
   shinyDebuggingPanel::withDebuggingPanel() ,
+  
   fluidRow(
-    
-    column(6, plotOutput("ThresholdContour")), 
-    column(6, plotOutput("linePlot")
-           , wellPanel(fluidRow(column(1,  HTML("Line thickness<br>controls")), column(1, ""),
-                                linethicknessButtons))
-    )
-  ),
-  fluidRow(
-    column(6, 
+    column(5, 
+           h2("Contour plot for thresholds", style='color:blue'),
            fluidRow(
-             column(4, 
-                    numericInput(inputId = "populationNumber", "# of Populations", value = 1)),
-             column(4, 
-                    numericInput(inputId = "thisPopulation", "This Population", value = 1)),
-             column(4,
-                    numericInput(inputId = "thisPopFraction", "This Population Fraction", value = 1)
-             ),
-             
-             fluidRow(
-               column(2, 
-                      numericInput(inputId = "popFractionFollows", "Which Population Fraction Follows", value = 1)),
-               column(2,
-                      numericInput(inputId = "thetaRmean", "Theta R Mean", value= 282)),
-               column(2,
+             plotOutput("ThresholdContour", click=clickOpts(id = 'click-threshold'))), 
+           h3("Controller for thresholds", style="text-align:center; color:blue"),
+           fluidRow(
+             column(3, style='background-color:lightgray;',
+                    numericInput(inputId = "populationNumber", HTML("<br># of Populations"), value = 1)),
+             column(3, style='background-color:lightgray;',
+                    numericInput(inputId = "thisPopulation", HTML("<br>This Population"), value = 1)),
+             column(3,style='background-color:lightgray;',
+                    numericInput(inputId = "thisPopFraction", "This Population Fraction", value = 1)),
+             column(3, style='background-color:lightgray;',
+                    numericInput(inputId = "popFractionFollows", "Which Pop'n Fraction Follows", value = 1))
+           ),
+           hr(),
+           div(
+             column(2,style='background-color:lightgray',
+                    numericInput(inputId = "thetaRmean", "Theta R Mean", value= 282)),
+               column(2,style='background-color:lightgray',
                       numericInput(inputId = "thetaR.CV", "Theta R CV", value = .8)),
-               column(2,
-                      numericInput(inputId = "correlation", "Correlation", value = .8)),
-               column(2,
+               column(2,style='background-color:lightgray',
+                      numericInput(inputId = "correlation", HTML("<br>Correlation"), value = .8)),
+               column(2,style='background-color:lightgray',
                       numericInput(inputId = "thetaTmean", "Theta T Mean", value = 447)),
-               column(2,
+               column(2,style='background-color:lightgray',
                       numericInput(inputId = "thetaT.CV", "Theta T CV", value = .8))
-             ),
-             
-             fluidRow(
-               column(6,
-                      numericInput(inputId = "probRefractory", "Pr(refractorytumor)", value = .85)),
-               column(6,
-                      numericInput(inputId = "responseLimitingTox", "K(response-limiting toxicity", value = .6))
-             )
-             
+           ),
+           
+           fluidRow(
+             column(6,
+                    numericInput(inputId = "probRefractory", "Pr(refractorytumor)", value = .85)),
+             column(6,
+                    numericInput(inputId = "responseLimitingTox", "K(response-limiting toxicity", value = .6))
            )
-    ), 
-    column(6, 
+    )
+    , 
+    column(1, 
+           div(style=paste0(
+             "border-left:1px solid #000;height:1500px;",
+             "border-right:1px solid #000;height:1500px;"),
+             br(), br(), br(), br(),
+             div(style='text-align:center; color:lightgreen;',
+                 numericInput('favoriteDose', 'selected dose', value=100, min=0))
+             # numericInput('favoriteDose', HTML("div('selected dose',
+             #   style='text-align:center; color:blue;'"), value=100, min=0)
+           )
+    ),
+    column(5
+           , h2("Probabilities and Expected Utility, E(U)", style="color:blue")
+           , wellPanel(fluidRow(column(2,  HTML("Line thickness controls")), 
+                                linethicknessButtons))
+           , plotOutput("linePlot")
+           ,
            div(
              br(),
-             h2("Controller for Utility Values", style="text-align:center; color:blue"),
-             fluidRow(           
-               column(6, h3("Enter Custom Values Below:", style="text-align:center; color:blue")),
-               column(6, h3("Or choose a Preset Option", style="text-align:center; color:blue"))
-             )),
-           fluidRow(
+             h3("Controller for utility values", style="text-align:center; color:blue"),
+             div(style='background-color:lightgrey;',
+               fluidRow(           
+                 column(6, h3("Enter custom values Below:", style="text-align:center; color:blue")),
+                 column(6, h3("Or choose a preset option", style="text-align:center; color:blue"))
+             ),
              fluidRow(
-               column(4, h2("t", style="text-align:center;")),
-               column(2, h2("T", style="text-align:center;")),
-               column(offset=6.5, width = 3, HTML("&nbsp;"),
-                      bsButton(inputId="Additive", 
-                               HTML("Additive<br>R=+1, T=-1"))
-               )),
+               fluidRow(
+                 column(4, h2("t", style="text-align:center;")),
+                 column(2, h2("T", style="text-align:center;")),
+                 column(offset=6.5, width = 3, HTML("&nbsp;"),
+                        bsButton(inputId="Additive", 
+                                 HTML("Additive<br>R=+1, T=-1"))
+                 )),
                column(1, h2("r")),
                column(2, 
                       tagAppendAttributes(
                         numericInput(inputId="U.rt", "U.rt", value=0),
-                      class='rtobj')),
+                        class='rtobj')),
+               column(2, offset=1,
+                      tagAppendAttributes(
+                        numericInput(inputId="U.rT", "U.rT", value=-1),
+                        class='rTobj')),
+               column(4,
+                      br(), 
+                      span(class='rTobj', '⬅︎') ,   
+                      # LEFTWARDS ARROW
+                      # Unicode: U+2190, UTF-8: E2 86 90,
+                      tagAppendAttributes(
+                        bsButton(inputId="Simple", HTML("Simple<br>U.rT=0")),
+                        class='rTobj')
+               )
+             ),
+             br(),
+             column(1, h2("R")),
+             column(2, 
+                    tagAppendAttributes(
+                      numericInput(inputId="U.Rt", "U.Rt", value=1),
+                      class='Rtobj')),
              column(2, offset=1,
                     tagAppendAttributes(
-                      numericInput(inputId="U.rT", "U.rT", value=-1),
-                      class='rTobj')),
-             column(4,
-                    br(), 
-                    span(class='rTobj', '⬅︎') ,   
-                    # LEFTWARDS ARROW
-                    # Unicode: U+2190, UTF-8: E2 86 90,
+                      numericInput(inputId="U.RT", "U.RT", value=0),
+                      class='RTobj'))
+             ,
+             column(4, 
+                    span(class='RTobj', '⬋', style="font-size:200%;") ,   #SOUTH WEST BLACK ARROW Unicode: U+2B0B, UTF-8: E2 AC 8B)
                     tagAppendAttributes(
-                      bsButton(inputId="Simple", HTML("Simple<br>U.rT=0")),
-                      class='rTobj')
+                      bsButton(inputId="Cautious", HTML("Cautious<br>U.RT=-1")),
+                      class='RTobj'),
+                    br(),
+                    span(class='RTobj', '⬉', style="font-size:200%;") ,  #NORTH WEST BLACK ARROW  Unicode: U+2B09, UTF-8: E2 AC 89
+                    tagAppendAttributes(
+                      bsButton(inputId="Aggressive", HTML("Aggressive<br>U.RT=+1")),
+                      class='RTobj')
              )
-           ),
-           br(),
-           column(1, h2("R")),
-           column(2, 
-                  tagAppendAttributes(
-                    numericInput(inputId="U.Rt", "U.Rt", value=1),
-                    class='Rtobj')),
-           column(2, offset=1,
-                  tagAppendAttributes(
-                    numericInput(inputId="U.RT", "U.RT", value=0),
-                    class='RTobj'))
-           ,
-           column(4, 
-                  span(class='RTobj', '⬋', style="font-size:200%;") ,   #SOUTH WEST BLACK ARROW Unicode: U+2B0B, UTF-8: E2 AC 8B)
-                  tagAppendAttributes(
-                    bsButton(inputId="Cautious", HTML("Cautious<br>U.RT=-1")),
-                    class='RTobj'),
-                  br(),
-                  span(class='RTobj', '⬉', style="font-size:200%;") ,  #NORTH WEST BLACK ARROW  Unicode: U+2B09, UTF-8: E2 AC 89
-                  tagAppendAttributes(
-                    bsButton(inputId="Aggressive", HTML("Aggressive<br>U.RT=+1")),
-                    class='RTobj')
            )
-    )
+         )
+  )
   )
 )
+
+
 
 
 server <- function(input, output, session) {
