0af7858aec4f9a4a2e228cf17be39f97b99d3842
cosbbi <mgerges19@oaklandcatholic.org>
2017-07-14 14:26:13 -0400

    Changed the.means.pop to the.logmedians.pop.

--- R/FromLognormalToNormal.R
+++ R/FromLognormalToNormal.R
@@ -1,7 +1,7 @@
 FromLognormalToNormal <-
 function(uStar, vStar) {
 	### Converts from the hidden parameters to the visible ones.  Typical arguments are:
-	# the.means.pop
+	# the.the.logmedians.pop
 	# the.variances.pop 
 	c(	mean=exp(uStar + vStar/2), 
 		cv=sqrt(exp(vStar) - 1))
