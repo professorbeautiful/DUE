5378202b0a5fe8e628f9d497e312fe88e3dc73ce
cosbbi <mgerges19@oaklandcatholic.org>
2017-07-14 14:25:43 -0400

    Changed the.means.pop to the.logmedians.pop.

--- R/plotThresholdContour.R
+++ R/plotThresholdContour.R
@@ -7,8 +7,8 @@ plotThresholdContour = function (plot.title = "Contour plot for thresholds", the
     the.grid = as.matrix(expand.grid(log10(DUEenv$doseValues),
         log10(DUEenv$doseValues)))
     the.dmvnorms = apply(as.array(1:DUEenv$nPops), 1, function(i) {
-        #cat("plotThresholdContour: the.means.pop[[i]] = ", DUEenv$the.means.pop[[i]], "\n")
-        return(DUEenv$proportions[i] * dmvnorm(the.grid, mean = DUEenv$the.means.pop[[i]]/log(10),
+        #cat("plotThresholdContour: the.logmedians.pop[[i]] = ", DUEenv$the.logmedians.pop[[i]], "\n")
+        return(DUEenv$proportions[i] * dmvnorm(the.grid, mean = DUEenv$the.logmedians.pop[[i]]/log(10),
             sigma = DUEenv$the.variances.pop[[i]]))
     })
     the.dmvnorms = array(the.dmvnorms, dim = c(nrow(the.grid),
