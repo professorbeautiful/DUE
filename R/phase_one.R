97c88d57e8f822f8f869b1752639277893003f99
Roger Day <notsofeo@gmail.com>
2017-07-27 10:18:29 -0400

    improved phase_one_exact.  still not quite right.

--- R/phase_one.R
+++ R/phase_one.R
@@ -1,40 +1,28 @@
-MAXLEVEL=20;
-N1 = 3;
-N2 = 3;
-MaxGo1 = 0;
-MinStop1 = 2;
-MaxToxGo = 1;
-PrLength = 2;
-verticalOffset = 60;
-PrTox =  p_stop = p_go_on = p_sum = rep(NA, MAXLEVEL);
-PrTox[1] = 0.1;
-PrTox[2] = 0.2;
-
-probbnml = function( pr,n, m)
-  pbinom(m, n, pr)
-
-run_phase_one = function(PrTox) {
+phase_one_exact = function(PrTox = c(.1,.2,.3,.4), N1=3,N2=3, MaxGo1 = 0, MinStop1 = 2, MaxToxGo = 1) {
+  pr_stop_at = pr_enter_tier = pr_Go_1 = pr_Go_2 = pr_Term_1 = rep(NA, length(PrTox));
   level=1 ;
-  p_go_on[level]=1; 
-  p_sum[level]=0;
+  pr_enter_tier[level]=1; 
   for ( level in 2:length(PrTox)){
-    p_Go_1 = probbnml(PrTox[level-1],N1,MaxGo1);
-    p_Term_1 = 1 - probbnml(PrTox[level-1],N1,MinStop1-1);
-    p_Go_2 = 0;
-    for (k in (MaxGo1+1):(MinStop1-1)){   ### k is #DLT in the unsure range
-      p_Tent = dbinom(k, N1, PrTox[level-1])  ### Pr we got k 
-      # Will we get few enough to continue?
-      maxNumberOK = MaxToxGo -(MaxGo1+1)
-      p_Go_2 = p_Go_2 +  p_Tent * probbnml(PrTox[level-1], N2, maxNumberOK);
-    }
-    p_stop[level] = ( 1- ( p_Go_1 + p_Go_2)) * p_go_on[level-1];  ## Marginal prob.
-    p_go_on[level] = ( p_Go_1 + p_Go_2) * p_go_on[level-1];   ## Marginal prob. 
-    p_sum[level] = p_sum[level-1] + p_stop[level]; 
+    pr_Go_1[level] = pbinom(q = MaxGo1, size = N1, prob = PrTox[level]);
+      # Pr(continue to the next cohort after the first half of the cohort | enter tier)
+    pr_Term_1[level] = 1 - pbinom(MinStop1-1, N1, PrTox[level]);
+      # Pr(terminate in the first half of the cohort | enter tier)
+    
+    k_neither = (MaxGo1+1):(MinStop1-1)
+      # these are the first-group toxicity counts leaving the decision unclear. Default: k=1.
+    pr_k_if_neither = dbinom(k_neither, N1, PrTox[level])
+    maxOK_2 = MaxToxGo - k_neither    
+      ### how many extra toxicities will be OK and still allow escalation.
+      # For the default, this is 1 minus 1 = zero. 
+    pr_ok_to_go =  dbinom(maxOK_2, N2, PrTox[level]) 
+    # these are the probabilities of the ambiguous toxicity counts. Default: k=1.
+    pr_Go_2[level] = sum(pr_k_if_neither * pr_ok_to_go )
+      # Pr( entering 2nd half and then escalating | enter tier)
+    
+    pr_stop_at[level] = ( 1- ( pr_Go_1[level] + pr_Go_2[level])) * pr_enter_tier[level];  ## Marginal prob.
+    if(level < length(PrTox))
+       pr_enter_tier[level+1] = ( pr_Go_1[level] + pr_Go_2[level]) * pr_enter_tier[level];   ## Marginal prob. 
   }
-  p_go_on[level] = p_go_on[level-1];
-  p_stop[level] = p_go_on[level];
-  p_sum[level] =  p_sum[level-1] + p_stop[level];
-  for(level in 1:length(PrTox))
-    cat(" level ", level, "p_go_on", p_go_on[level], '\n',
-        "  P_stop=", p_stop[level], '\n');
+  result = cbind(pr_enter_tier, pr_Go_1, pr_Term_1, pr_Go_2, pr_stop_at)
+  return(result)
 }
