0e5d399fa131437a346fb777d89ecf6ee72e053f
Roger Day <notsofeo@gmail.com>
2017-08-03 16:30:03 -0400

    We like the middel column now!! Don't we?

--- inst/DUEshiny/app.R
+++ inst/DUEshiny/app.R
@@ -97,30 +97,35 @@ ui <- fluidPage(
            , 
            ####  MIDDLE: Doses and Files ####
            column(1, 
+                  br(), br(),
+                  br(), br(),
+                  br(), br(),
+                  br(), 
                   div(style=paste0(
+                    "vertical-align:center;",
                     "border-left:1px solid #000;",
+                    "border-bottom:1px solid #000;",
+                    "border-top:1px solid #000;",
                     "border-right:1px solid #000;"),  ### height:1500px;
                     # See also https://stackoverflow.com/questions/571900/is-there-a-vr-vertical-rule-in-html
                     # especially the display:flex solution.
-                    br(),
                     div(style='text-align:center; color:white; border-color:darkgreen; background-color:green;',
                         numericInput('favoriteDose', 'Selected dose', value=100, min=0)),
                     br(), br(),
-                    fluidRow(style = "font-size:medium", 
+                    div(style = "background-color:green;", 
                              #column(2, 
-                             bsButton("changeAxes", "Change axes", size = 'medium')
+                             bsButton("changeAxes", HTML("Change <br> dose<br>axes"))
                              #)
                     ),
                     br(),
                     br(),
-                    hr(),
                     br(),
                     ####phase1resultbutton####
                     div(style=paste0("background-color:", "red"),
                         bsButton(inputId = "phase1ResultButton", label = HTML("Phase I <br> Results")
                         )
-                    ),
-                    hr(style = 'margin-top: 0.5em; margin-bottom: 0.5em; border-style:inset; border-width: 2px')
+                    )
+                    # hr(style = 'margin-top: 0.5em; margin-bottom: 0.5em; border-style:inset; border-width: 2px')
                     # fluidRow(style =  "font-size:large",
                     #          bsButton(inputId = "load", label = HTML("Load saved <br> parameters<br>(new window)"), size = 'medium')
                     # ),
