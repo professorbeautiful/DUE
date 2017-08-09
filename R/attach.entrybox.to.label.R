f9c3545531f2e8955ff3925081221dac936bf8d6
Roger Day <notsofeo@gmail.com>
2017-08-08 23:19:26 -0400

    Deleting TK stuff not needed.

--- R/attach.entrybox.to.label.R
+++
@@ -1,7 +0,0 @@
-attach.entrybox.to.label <-
-function(the.label, the.entrybox) {
-	tkplace(the.entrybox, 
-		"-x", add(get.tkplaceinfo.x(the.label), get.tkplaceinfo.width(the.label)),
-		"-y", get.tkplaceinfo.y(the.label), "-height", 20, "-width", 35)
-}
-
--- R/read.Uvalues.R
+++
@@ -1,9 +0,0 @@
-read.Uvalues <-
-function() {
-	utility.new = sapply( 
-			paste("U.", c("rt", "rT", "Rt", "RT"), sep=""),
-		function(uname) as.numeric(tclvalue( uname )))
-	DUEenv$utility <- utility.new
-#	cat("New Uvalues are ", DUEenv$utility, "\n")
-}
-
