495e06931c6ac9ba44cb8b2b1ac935c74528fa95
Roger Day <notsofeo@gmail.com>
2018-01-05 16:43:59 -0500

    This is where DUE_vignette_plottingFunctions.R should be.

---
+++ inst/doc/DUE_vignette_plottingFunctions.R
@@ -0,0 +1,39 @@
+## @knitr plottingFunctions
+##  ```{r ref.label=plottingFunctions}
+##  source()  works! Skip the knitr:read_chunk way.
+ 
+envHolder = new.env()
+getExampleFile = function(string) {
+  dirString = ifelse(basename(getwd()) == "www", '../', '../inst/DUEshiny/')
+  filename = paste0(dirString,  rev(grep(value=TRUE, string,
+                                         dir(dirString)))[1]) 
+  if(class(filename) == 'try-error') {
+    cat("file not found ", string, "\n")
+    print(getwd())
+    print(dir())
+  }
+  filename
+}
+
+plotThresholdInVignette= function(string) {
+  filename = getExampleFile(string) 
+  if(class(filename) != 'try-error') {
+    load(filename, envir = envHolder)
+    rt.outcome.colors = envHolder$rt.outcome.colors
+    try({
+      plotThresholdContour(theDUEenv = envHolder$DUEsaving, context='vignette')
+      mtext(side = 3, string, cex=1)
+    })
+  }
+}
+plotEUprobsInVignette= function(string) {
+  filename = getExampleFile(string) 
+  if(class(filename) != 'try-error') {
+    load(filename, envir = envHolder)
+    rt.outcome.colors = envHolder$rt.outcome.colors
+    try({
+      plotProbsAndEU(DUEenv = envHolder$DUEsaving, context='vignette')
+      mtext(side = 3, string, cex=1)
+    })
+  }
+}
