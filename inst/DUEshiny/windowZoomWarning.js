3a4bc4f272cde9309d66e0cdd8707a3100d9973e
Roger Day <notsofeo@gmail.com>
2018-02-03 12:58:04 -0500

    Passes the value of innerWidth to the input$ structure in R,  for use in renderPlot calls.

--- inst/DUEshiny/windowZoomWarning.js
+++ inst/DUEshiny/windowZoomWarning.js
@@ -1,6 +1,17 @@
-// This works when the app is first opened.
-
-
+// See
+// https://stackoverflow.com/questions/40538365/r-shiny-how-to-get-square-plot-to-use-full-width-of-panel/40539526#40539526
+//  Passes the value of innerWidth to the input$ structure in R,
+// for use in renderPlot calls.
+function innerWidthHandler(e) {
+    Shiny.onInputChange("innerWidth", window.innerWidth);
+}
+$(document).on(
+  "shiny:connected", 
+  innerWidthHandler);
+$(window).resize( 
+  innerWidthHandler );
+                            
+ 
 var snap = function (r, snaps, ratios)
 {
   var i;
@@ -24,4 +35,3 @@ if(window.innerWidth < 2500) {
   alert('Window zoom is too high. For best appearance, please zoom out (e.g. on OSX, Cmd-minus).');
 }
 
-/* jQuery("body").css("zoom", "50%") */
