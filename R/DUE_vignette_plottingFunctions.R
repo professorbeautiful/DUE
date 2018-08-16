9c6f4df0c7ceddf3657969a86cfc7b9a2ffb62da
Roger Day <notsofeo@gmail.com>
2018-08-15 22:08:37 -0400

    Moving DUE_vignette_plottingFunctions.R into R folder.

--- R/DUE_vignette_plottingFunctions.R
+++ R/DUE_vignette_plottingFunctions.R
@@ -2,31 +2,28 @@
 ##  ```{r ref.label=plottingFunctions}
 ##  source()  works! Skip the knitr:read_chunk way.
  
-envHolder = new.env()
 getExampleFile = function(string) {
-  dirString = ifelse(basename(getwd()) == "www", '../', '../inst/DUEshiny/')
-  filename = paste0(dirString,  rev(grep(value=TRUE, string,
+  dirString = system('find .. -name DUEshiny', intern = T)
+  print(dirString)
+  # ifelse(basename(getwd()) == "www", '../', '../inst/DUEshiny/')
+  filename = paste0(dirString,  '/', rev(grep(value=TRUE, string,
                                          dir(dirString)))[1]) 
-  if(class(filename) == 'try-error') {
-    cat("file not found ", string, "\n")
-    print(getwd())
-    print(dir())
-  }
+  print(filename)
   filename
 }
 
 plotThresholdInVignette= function(string) {
+  envHolder = new.env()
   filename = getExampleFile(string) 
-  if(class(filename) != 'try-error') {
-    load(filename, envir = envHolder)
-    rt.outcome.colors = envHolder$rt.outcome.colors
-    try({
-      plotThresholdContour(theDUEenv = envHolder$DUEsaving, context='vignette')
-      mtext(side = 3, string, cex=1)
-    })
-  }
+  load(filename, envir = envHolder)
+  rt.outcome.colors = envHolder$rt.outcome.colors
+  try({
+    plotThresholdContour(theDUEenv = envHolder$DUEsaving, context='vignette')
+    mtext(side = 3, string, cex=1)
+  })
+  return(envHolder)
 }
-plotEUprobsInVignette= function(string) {
+plotEUprobsInVignette= function(string, envHolder) {
   filename = getExampleFile(string) 
   if(class(filename) != 'try-error') {
     load(filename, envir = envHolder)
