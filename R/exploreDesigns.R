c6d431be8c477e32369de50547280d1fca9394a4
Roger Day <notsofeo@gmail.com>
2019-09-02 21:19:54 -0400

    designParameterNames and designParameterNamesExpanded

--- R/exploreDesigns.R
+++ R/exploreDesigns.R
@@ -6,10 +6,10 @@
 #  recalculate.offdiagonals() ... when the correlation is changed.
 # recalculate.means.and.variances() ... creates the.variances.pop
 
-designParameters = 
+designParameterNames = 
   strsplit(
     split = "[ \n]",
-'utilities doseValues
+'utility doseValues
 nPops the.CVs.pop the.correlations.pop the.medianThresholds.pop
 refractory Kdeath proportions
 '  
@@ -26,15 +26,39 @@ get(env=DUEsaving, 'the.variances.pop')
   ## Off diagonals from the.correlations.pop and the diagonals.
 get(env=DUEsaving, 'theLognormalParameters')  # omit
 
+lapply(designParameterNames, function(p)
+  (get(p, env=DUEsaving)) )
+
+### Predicated on the value of nPops.
+designParameterNamesExpanded = 
+  c(names(DUEsaving$utility),
+    paste0(
+      paste0('the.CVs.pop[[', 1:DUEsaving$nPops, ']]'),
+      '[', c('R','T'), ']')
+    ,
+    paste0(
+      paste0('the.medianThresholds.pop[[', 1:DUEsaving$nPops, ']]'),
+      '[', c('R','T'), ']')
+    ,
+    paste0('the.correlations.pop[', 1:DUEsaving$nPops, ']')
+    ,
+    
+    'refractory',
+    'Kdeath',
+    paste0('proportions[', 1:DUEsaving$nPops, ']')
+  )
+
+ 
+  
 eachRow = function(row){
   arglist = design[row, ]
-  for(arg in names(arglist))  DUEenv[[arg]] = arglist[[arg]]
-  calculate.probabilities (DUEenv, log10dose=2, utility) 
+  for(arg in names(arglist))  DUEsaving[[arg]] = arglist[[arg]]
+  calculate.probabilities (DUEsaving, log10dose=2, utility) 
   ### TODO save extractUtilitySummaries
 }
 calculate.probabilities.design <- function(design, DUEenvRow=DUEinits.default, ...) {
   # For each row of the design matrix, place the values in calculate the 
   sapply(1:nrow(design), eachRow)
-  DUEenv$eightprobs <- 
-    sapply(DUEenv$log10doseValues, calculate.probabilities, DUEenv=DUEenv)
+  DUEsaving$eightprobs <- 
+    sapply(DUEsaving$log10doseValues, calculate.probabilities, DUEenv=DUEsaving)
 }
