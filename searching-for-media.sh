55ddb7551c544883a11aac109024b9a6f96cd0d9
Roger Day <notsofeo@gmail.com>
2024-06-22 11:54:13 -0400

    Camtasia doesn't support search of added media, so these kinds of shell commands might help.

---
+++ searching-for-media.sh
@@ -0,0 +1,6 @@
+ls 'DUE tour video, in progress.cmproj/media'/*|grep -v 'Pasted' \
+| grep -v 'DUE tour'| grep -v '^$' | grep -v '^deleted' \
+|  wc
+
+find 'DUE tour video, in progress.cmproj/media' -name 'frame*'
+find 'DUE tour video, in progress.cmproj/media' -name '*hase*'
