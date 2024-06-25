c2b53f421497fb2c812ee11724cdb61b7e613a50
Roger Day <notsofeo@gmail.com>
2024-06-25 16:06:27 -0400

    more in searching-for-media.sh

--- searching-for-media.sh
+++ searching-for-media.sh
@@ -2,5 +2,8 @@ ls 'DUE tour video, in progress.cmproj/media'/*|grep -v 'Pasted' \
 | grep -v 'DUE tour'| grep -v '^$' | grep -v '^deleted' \
 |  wc
 
+
 find 'DUE tour video, in progress.cmproj/media' -name 'frame*'
 find 'DUE tour video, in progress.cmproj/media' -name '*hase*'
+find 'DUE tour video, in progress.cmproj/media' -name 'ph1*'
+find '.' -name 'ph1*' ## in 'screenshots for DUE'
